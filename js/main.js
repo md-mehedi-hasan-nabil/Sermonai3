e5eb455ca6f7def1f7e96ecb503d68c3fdbba2f8
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2023-11-23 17:09:42 +0600

    remove console.log

--- js/main.js
+++ js/main.js
@@ -1,56 +1,51 @@
 AOS.init({ once: true, duration: 800 });
 
-      const footer = document.querySelector("footer");
+const footer = document.querySelector("footer");
 
-      document.addEventListener("scroll", (e) => {
-        if (window.innerWidth < 576) {
-          footer.style.backgroundPosition = "center";
-        } else {
-          footer.style.backgroundPosition = `0px ${
-            -(window.scrollY / 10) + 450
-          }px`;
-        }
-      });
+document.addEventListener("scroll", (e) => {
+  if (window.innerWidth < 576) {
+    footer.style.backgroundPosition = "center";
+  } else {
+    footer.style.backgroundPosition = `0px ${-(window.scrollY / 10) + 450}px`;
+  }
+});
 
-      const clientFeedbackSection = document.querySelector(
-        ".client-feedback-section"
-      );
-      const clientFeedbackIndicatorButton = document.querySelector(
-        ".client-feedback-indicator-button"
-      );
+const clientFeedbackSection = document.querySelector(
+  ".client-feedback-section"
+);
+const clientFeedbackIndicatorButton = document.querySelector(
+  ".client-feedback-indicator-button"
+);
 
-      const footerSection = document.querySelector(".footer-section");
-      const footerIndicatorButton = document.querySelector(
-        ".footer-indicator-button"
-      );
+const footerSection = document.querySelector(".footer-section");
+const footerIndicatorButton = document.querySelector(
+  ".footer-indicator-button"
+);
 
-      const observer1 = new IntersectionObserver((items) => {
-        items.forEach((item) => {
-          if (item.isIntersecting) {
-            console.log(item.target.className);
+const observer1 = new IntersectionObserver((items) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      if (item.target.className.includes("client-feedback-section")) {
+        clientFeedbackIndicatorButton.style.background = "#fff";
+      }
+    } else {
+      clientFeedbackIndicatorButton.style.background = "transparent";
+    }
+  });
+});
 
-            if (item.target.className.includes("client-feedback-section")) {
-              clientFeedbackIndicatorButton.style.background = "#fff";
-            }
-          } else {
-            clientFeedbackIndicatorButton.style.background = "transparent";
-          }
-        });
-      });
+observer1.observe(clientFeedbackSection);
 
-      observer1.observe(clientFeedbackSection);
+const observer2 = new IntersectionObserver((items) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      if (item.target.className.includes("footer-section")) {
+        footerIndicatorButton.style.background = "#fff";
+      }
+    } else {
+      footerIndicatorButton.style.background = "transparent";
+    }
+  });
+});
 
-      const observer2 = new IntersectionObserver((items) => {
-        items.forEach((item) => {
-          if (item.isIntersecting) {
-            console.log(item.target.className);
-
-            if (item.target.className.includes("footer-section")) {
-              footerIndicatorButton.style.background = "#fff";
-            }
-          } else {
-            footerIndicatorButton.style.background = "transparent";
-          }
-        });
-      });
-      observer2.observe(footerSection);
+observer2.observe(footerSection);
